5affed7c0974aadc2d4a9d62eac2f979bf3a3d2a
afnavarro27 <andresfelipenavarrovalencia@gmail.com>
2022-05-05 17:49:34 -0500

    ultima verson

---
+++ js/validacionLogin.js
@@ -0,0 +1,22 @@
+function validarLogin() {
+  let v1 = document.getElementById("usuarioLoguin").value;
+  let v2 = document.getElementById("contraseñaLoguin").value;
+  if (v1 == "usuario@gmail.com") {
+    if (v2 == "12345") {
+      window.alert("inicio de usuario");
+    }
+  }
+
+  if (v1 == "empleado@gmail.com") {
+    if (v2 == "123456") {
+      window.alert("inicio de Empleado");
+    }
+  }
+
+  if (v1 == "admin@gmail.com") {
+    if (v2 == "1234567") {
+      window.alert("inicio de Administrador");
+      prompt("hola");
+    }
+  }
+}
